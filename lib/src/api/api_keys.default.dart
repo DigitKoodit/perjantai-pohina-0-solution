81e36fca7040684661df9337afc5d8279b3b0fa5
Onnimanni Hannonen <51901607+O-Hannonen@users.noreply.github.com>
2024-04-05 19:04:36 +0300

    fix: fixes default api keys file

--- lib/src/api/api_keys.default.dart
+++ lib/src/api/api_keys.default.dart
@@ -6,5 +6,5 @@
 /// You can also specify an API Key via --dart-define. Example:
 /// "flutter run --dart-define API_KEY=YOUR_API_KEY
 class APIKeys {
-  static const String? openWeatherAPIKey = null;
+  static const openWeatherAPIKey = '';
 }
